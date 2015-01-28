02085a986b166dbc6b1069719fa8efc9686a18e0
Maunik Shah <mshah@codeaurora.org>
2015-01-28 16:09:31 +0530

    msm8610: init: Call eMMC recovery init
    Calling eMMC recovery common function in recovery code will give as
    a way to distinguish what booting to proceed. There are two options
    to boot - normal boot using kernel and ramdisk from boot partition
    and recovery boot using kernel and ramdisk from recovery eMMC
    partition. The decision is made from the common code in recovery.c
    checking for boot-recovery message from misc partition.
    
    Change-Id: I128a0e58bcb551af81af1740bb2aeeecb1e54b3c

--- target/msm8610/init.c
+++ target/msm8610/init.c
@@ -338,6 +338,11 @@ unsigned target_baseband()
 	return board_baseband();
 }
 
+int emmc_recovery_init(void)
+{
+    return _emmc_recovery_init();
+}
+
 void target_serialno(unsigned char *buf)
 {
 	uint32_t serialno;
