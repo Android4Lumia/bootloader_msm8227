0bde420117e206012330f821d3d07f2fdbd4e669
M1cha <sigmaepsilon92@gmail.com>
2015-09-04 13:52:02 +0200

    [EFIDROID] dev: keys: add key queue

--- dev/keys/keys.c
+++ dev/keys/keys.c
@@ -29,39 +29,124 @@
  * SUCH DAMAGE.
  */
 
+#include <err.h>
 #include <bits.h>
+#include <list.h>
 #include <debug.h>
 #include <string.h>
+#include <malloc.h>
 #include <dev/keys.h>
 
+#define LOCAL_TRACE 0
+
 static unsigned long key_bitmap[BITMAP_NUM_WORDS(MAX_KEYS)];
+
+typedef struct {
+	struct list_node node;
+	uint16_t code;
+	uint16_t value;
+} key_event_t;
+
+static struct list_node event_queue;
+static struct list_node event_sources;
+
+int keys_poll(void)
+{
+	key_event_source_t *source;
+	list_for_every_entry(&event_sources, source, key_event_source_t, node) {
+		// update time
+		time_t current = current_time();
+		source->delta = current-source->last;
+		source->last = current;
+
+		source->poll(source);
+	}
+
+	return 0;
+}
 
 void keys_init(void)
 {
 	memset(key_bitmap, 0, sizeof(key_bitmap));
+	list_initialize(&event_queue);
+	list_initialize(&event_sources);
 }
 
-void keys_post_event(uint16_t code, int16_t value)
+void keys_add_source(key_event_source_t* source) {
+	source->last = INFINITE_TIME;
+
+	list_add_tail(&event_sources, &source->node);
+}
+
+int keys_post_event(uint16_t code, int16_t value)
 {
 	if (code >= MAX_KEYS) {
-		dprintf(INFO, "Invalid keycode posted: %d\n", code);
-		return;
+		LTRACEF("Invalid keycode posted: %d\n", code);
+		return ERR_INVALID_ARGS;
 	}
 
-	/* TODO: Implement an actual event queue if it becomes necessary */
 	if (value)
 		bitmap_set(key_bitmap, code);
 	else
 		bitmap_clear(key_bitmap, code);
 
-//	dprintf(INFO, "key state change: %d %d\n", code, value);
+	// I guess we shouldn't ignore these
+	if(!value) return NO_ERROR;
+
+	// create event
+	key_event_t* event = malloc(sizeof(key_event_t));
+	event->code = code;
+	event->value = value;
+
+	// add event
+	list_add_tail(&event_queue, &event->node);
+
+	// signal
+	LTRACEF("key state change: %d %d\n", code, value);
+
+	return NO_ERROR;
 }
 
 int keys_get_state(uint16_t code)
 {
 	if (code >= MAX_KEYS) {
-		dprintf(INFO, "Invalid keycode requested: %d\n", code);
-		return -1;
+		LTRACEF("Invalid keycode requested: %d\n", code);
+		return ERR_INVALID_ARGS;
 	}
+
 	return bitmap_test(key_bitmap, code);
+}
+
+void keys_clear_all(void)
+{
+	memset(key_bitmap, 0, sizeof(key_bitmap));
+
+	while(keys_has_next()) {
+		key_event_t* event = list_remove_tail_type(&event_queue, key_event_t, node);
+		free(event);
+	}
+}
+
+int keys_get_next(uint16_t* code, uint16_t* value)
+{
+	// wait for next event
+	if(!keys_has_next())
+		return ERR_NOT_READY;
+
+	// get event
+	key_event_t* event = list_remove_head_type(&event_queue, key_event_t, node);
+
+	// set result code
+	*code = event->code;
+	*value = event->value;
+
+	// free event
+	free(event);
+
+	return NO_ERROR;
+}
+
+int keys_has_next(void)
+{
+	return !list_is_empty(&event_queue);
 }
--- include/dev/keys.h
+++ include/dev/keys.h
@@ -33,6 +33,8 @@
 #define __DEV_KEYS_H
 
 #include <sys/types.h>
+#include <list.h>
+#include <platform.h>
 
 /* these are just the ascii values for the chars */
 #define KEY_0		0x30
@@ -84,8 +86,70 @@
 
 #define MAX_KEYS	0x1ff
 
+typedef struct key_event_source key_event_source_t;
+typedef int (*key_event_poll_t)(key_event_source_t* source);
+
+typedef struct {
+	bool pressed;
+	time_t time;
+	bool repeat;
+} keymap_t;
+
+struct key_event_source {
+	struct list_node node;
+	void* pdata;
+
+	time_t last;
+	time_t delta;
+	key_event_poll_t poll;
+	keymap_t keymap[MAX_KEYS];
+};
+
 void keys_init(void);
-void keys_post_event(uint16_t code, int16_t value);
+int keys_poll(void);
+void keys_add_source(key_event_source_t* source);
+int keys_post_event(uint16_t code, int16_t value);
 int keys_get_state(uint16_t code);
+void keys_clear_all(void);
+int keys_get_next(uint16_t* code, uint16_t* value);
+int keys_has_next(void);
+
+static inline int keys_set_report_key(key_event_source_t* source, uint16_t code, uint16_t value) {
+	// update key time
+	source->keymap[code].time+=source->delta;
+
+	if(value) {
+		bool report = false;
+
+		// change to pressed
+		if(!source->keymap[code].pressed) {
+			report=true;
+			source->keymap[code].time=0;
+			source->keymap[code].pressed=true;
+		}
+
+		// key repeat
+		if((source->keymap[code].repeat && source->keymap[code].time>=200) || source->keymap[code].time>=500) {
+			report=true;
+			source->keymap[code].time=0;
+			source->keymap[code].repeat=true;
+		}
+
+		if(report){
+			 return 1;
+		}
+	}
+
+	// change to released
+	else if(source->keymap[code].pressed && source->keymap[code].time>200) {
+		source->keymap[code].pressed=false;
+		source->keymap[code].time=0;
+		source->keymap[code].repeat=false;
+
+		return 1;
+	}
+
+	return 0;
+}
 
 #endif /* __DEV_KEYS_H */
