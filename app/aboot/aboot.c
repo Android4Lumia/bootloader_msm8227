46068a40ee863da6936b3b436cc649bd50421c24
M1cha <sigmaepsilon92@gmail.com>
2015-12-08 21:01:12 +0100

    app: aboot: add fastboot command 'oem reboot-dload'

--- app/aboot/aboot.c
+++ app/aboot/aboot.c
@@ -2834,6 +2834,20 @@ void cmd_oem_reboot_recovery(const char *arg, void *data, unsigned sz)
 	reboot_device(RECOVERY_MODE);
 }
 
+void cmd_oem_reboot_dload(const char *arg, void *data, unsigned sz)
+{
+	fastboot_okay("");
+	if (set_download_mode(EMERGENCY_DLOAD))
+	{
+		dprintf(CRITICAL,"dload mode not supported by target\n");
+	}
+	else
+	{
+		reboot_device(DLOAD);
+		dprintf(CRITICAL,"Failed to reboot into dload mode\n");
+	}
+}
+
 void cmd_oem_enable_charger_screen(const char *arg, void *data, unsigned size)
 {
 	dprintf(INFO, "Enabling charger screen check\n");
@@ -3553,6 +3567,7 @@ void aboot_fastboot_register_commands(void)
 											{"reboot", cmd_reboot},
 											{"reboot-bootloader", cmd_reboot_bootloader},
 											{"oem reboot-recovery", cmd_oem_reboot_recovery},
+											{"oem reboot-dload", cmd_oem_reboot_dload},
 											{"oem unlock", cmd_oem_unlock},
 											{"oem unlock-go", cmd_oem_unlock_go},
 											{"oem lock", cmd_oem_lock},
